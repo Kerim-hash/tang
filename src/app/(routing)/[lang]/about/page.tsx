bc078549efcda980255d49779315336d7338f8b6
kerim-hash <kerimnurkaliev3@gmail.com>
2024-12-13 17:49:49 +0600

    added meta

--- src/app/(routing)/[lang]/about/page.tsx
+++ src/app/(routing)/[lang]/about/page.tsx
@@ -1,6 +1,17 @@
 import { AboutPage } from '@/_pages';
 import Meta from '@/widgets/Meta';
+import { Metadata } from 'next';
 
+export const metadata: Metadata = {
+	
+	icons: { icon:'/_next/static/media/favicon.ico',},
+	title:
+	  "Суррогатное материнство | О нас",
+	description:
+	  "Центр Motherhood предлагает безопасные и качественные услуги суррогатного материнства для семей по всему миру. Узнайте больше о наших услугах!",
+  };
+
+  
 const page = () => {
 	return <>
 		<AboutPage />
